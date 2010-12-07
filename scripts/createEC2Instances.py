ac36a1364ae7474a33fba65782588e64c0eec222
Patrick Horn <patrick.horn@gmail.com>
2010-12-06 23:42:50 -0800

    Added new arguments to the create script to make it work better over ssh

--- scripts/createEC2Instances.py
+++ scripts/createEC2Instances.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 from __future__ import with_statement
 
@@ -9,9 +8,9 @@ import optparse
 from subprocess import Popen, PIPE
 
 def getInstanceInfo(cluster=None):
-           pipe = Popen("ec2-describe-instances", stdout=PIPE)
-    output = pipe.communicate()[0]
-    """RESERVATION	r-bb214bd1	051423782292	Cassandra
+    #pipe = Popen("ec2-describe-instances", stdout=PIPE)
+    #output = pipe.communicate()[0]
+    output="""RESERVATION	r-bb214bd1	051423782292	Cassandra
 INSTANCE	i-d8c79db5	ami-2272864b	ec2-67-202-15-186.compute-1.amazonaws.com	ip-10-242-49-18.ec2.internal	running	patrick	0		t1.micro	2010-12-06T23:34:32+0000	us-east-1a	aki-427d952b		monitoring-disabled	67.202.15.186	10.242.49.18			ebs	paravirtual	
 BLOCKDEVICE	/dev/sda1	vol-fb9e5c93	2010-12-06T23:34:44.000Z	
 TAG	instance	i-d8c79db5	Cluster	test
@@ -33,7 +32,7 @@ TAG	instance	i-d8c79db5	Node	0
             extip = info[3] #info[13]
             intip = info[4] #info[14]
             user = "ec2-user"
-            instances[id] = {"tag": {}, "extip": extip, "intip": intip, "datacenter": datacenter, "state": state, "username": user}
+            instances[id] = {"tag": {}, "extip": extip, "intip": intip, "datacenter": datacenter, "state": state, "user": user}
         if info[0]=="TAG":
             id = info[2]
             key = info[3]
@@ -46,18 +45,20 @@ def createInstances(basedir, instInfo, certfile):
         os.mkdir(basedir)
     except OSError:
         pass
-    sshHosts = ["%s@%s"%(noe["user"],node["extip"]) for node in instInfo.values()]
+    sshHosts = ["%s@%s"%(node["user"],node["extip"]) for node in instInfo.values()]
     allNodes = ["%s"%(node["extip"]) for node in instInfo.values()]
-    for extip, host in zip(extips, sshHosts):
-        os.spawnlp(os.P_WAIT,"ssh", "-i", certfile, host,
-		   "cd cassandra && git fetch origin && git pull origin master && python scripts/create.py --btport 6881 -n "+(','.join(allNodes))+" -d ~/"+basedir)
+    for sshHost in sshHosts:
+        args = ["ssh", "ssh", "-i", certfile, sshHost,
+                "cd poseidon && git fetch origin && git pull origin master && python scripts/create.py --btport 6881 -n "+(','.join(allNodes))+" -d ~/"+basedir]
+        print args
+        os.spawnlp(os.P_WAIT, *args)
 
     create.setupDirectory("%s/cli"%(basedir,),
-			  create.Port(8000, "127.0.0.1", 6881),
-			  allNodes,
+			  create.Port(8000, "127.0.0.1", create.BT_PORT),
+			  [create.Port(create.DEFAULT_PORT, n, create.BT_PORT) for n in allNodes],
 			  isCli=True)
 
-    startAllScript = "%s/startall.sh"%(basedir, )
+    startAllScript = "%s/startall.sh"%(basedir,)
     with open(startAllScript, 'w') as script:
         script.write("""#!/bin/bash
 for sshHost in %s; do
@@ -76,14 +77,14 @@ done
 
 if __name__=="__main__":
     parser = optparse.OptionParser()
-    parser.add_option("-d", "--dir", dest="dir", help="Top-level directory for all nodes")
+    parser.add_option("-d", "--dir", dest="dir", help="Top-level directory for all nodes. *MUST* be relative path")
     parser.add_option("-c", "--clusterid", default=None, dest="clusterid", help="Cluster ID (default 0).")
     parser.add_option("-k", "--key", "--cert", dest="certfile", help="SSH Private Key (.pem)")
     parser.add_option("-n", "--nocreate", dest="nocreate", action="store_true", help="Just get node list")
     (options, args) = parser.parse_args()
 
     if not options.dir or not options.certfile:
-        print >>sys.stderr, "Must specify number of nodes (-n) and directory (-d)"
+        print >>sys.stderr, "Must specify private key (-k) and directory (-d)"
         parser.print_help()
         sys.exit(1)
 
@@ -91,8 +92,8 @@ if __name__=="__main__":
     print "Instances:"
     for nodename,dic in instanceInfo.iteritems():
         print "Name: %s" % (nodename,)
-        for k,v in dic:
+        for k,v in dic.iteritems():
             print "    %s: %s"%(k,v)
     if not options.nocreate:
-        createInstances(options.dir, instnaceInfo, options.certfile)
+        createInstances(options.dir, instanceInfo, options.certfile)
 
