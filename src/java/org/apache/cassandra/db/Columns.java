d19debaaad56b0dc56b125078b2511dfb6f6811c
James Ide <ide+github@jameside.com>
2010-12-10 16:36:20 -0800

    Un-Thrifting column methods added to Columns.java

---
+++ src/java/org/apache/cassandra/db/Columns.java
@@ -0,0 +1,31 @@
+package org.apache.cassandra.db;
+
+import org.apache.cassandra.db.marshal.AbstractType;
+
+public class Columns {
+
+    private Columns() { }
+
+    public static IColumn fromThrift(
+            org.apache.cassandra.thrift.ColumnOrSuperColumn cosc,
+            AbstractType subComparator) {
+        return (cosc.column == null) ?
+               fromThrift(cosc.super_column, subComparator) :
+               fromThrift(cosc.column);
+    }
+
+    public static IColumn fromThrift(
+            org.apache.cassandra.thrift.SuperColumn superColumn,
+            AbstractType subComparator) {
+        SuperColumn sc = new SuperColumn(superColumn.name, subComparator);
+        for (org.apache.cassandra.thrift.Column column : superColumn.columns) {
+            sc.addColumn(fromThrift(column));
+        }
+        return sc;
+    }
+
+    private static IColumn fromThrift(
+            org.apache.cassandra.thrift.Column column) {
+        return new Column(column.name, column.value, column.timestamp);
+    }
+}
