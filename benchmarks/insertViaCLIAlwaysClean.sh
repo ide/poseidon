7b45caf6b71c660de1e7d43b3d4b81a4914a3a7e
mixahli <mixahli@ubuntu>
2010-12-15 18:29:14 -0800

    Added a script that cleans the servers between each time measurement. (DB is always clean).

--- benchmarks/insertViaCLIAlwaysClean.sh
+++ benchmarks/insertViaCLIAlwaysClean.sh
@@ -35,7 +35,7 @@ function timed_cmd () {
 DATADIR=$PWD/testdata
 Iterations="1 2 3 4 5"
 #SIZES="1 4 16 64"
-SIZES="16k 32k 64k 128k"
+SIZES="8k 16k 32k 64k"
 
 function cleanup() {
   rm -f ec2/cli/torrents/*
@@ -43,6 +43,13 @@ function cleanup() {
   rm -f ec2/cli/*.dat*
   rm -rf ec2/active-data/*
   cp -rf poseidon/active-data/* ec2/active-data/
+}
+
+function remote_cleanup() {
+  ec2/restartall --delete
+#  ec2/stopall.sh --delete
+#  ec2/commandonall.sh 'killall -9 java utserver; rm -f ec2/downloads/*; rm -f ec2/torrents/*; rm -rf ec2/active-data/*; cp -rf poseidon/active-data/* ec2/active-data/; rm -f ec2/*.dat*' 
+#  ec2/startall.sh
 }
 
 cleanup
@@ -59,7 +66,9 @@ for i in $Iterations; do
     echo "Normal Columns: $normalColumns"
     echo "Torrent Columns: $torrentColumns"
 
+    remote_cleanup
     normalDuration=$(timed_cmd "set Keyspace1.Standard1['benchmark_${i}_${kbytes}']['${normalColumns}'] = '${value}' ALL")
+    remote_cleanup
     torrentDuration=$(timed_cmd "set Keyspace1.Standard1['benchmark_${i}_${kbytes}']['${torrentColumns}'] = '${value}' ALL")
 
     echo -e "set\t$kbytes\t$normalDuration\t$torrentDuration"
@@ -72,7 +81,11 @@ for i in $Iterations; do
     normalColumns=$(get_column_names _F ${DATADIR}/${kbytes})
     torrentColumns=$(get_column_names __T_F ${DATADIR}/${kbytes})
 
+    remote_cleanup
+    $(timed_cmd "set Keyspace1.Standard1['benchmark_${i}_${kbytes}']['${normalColumns}'] = '${value}' ALL")
     normalDuration=$(timed_cmd "get Keyspace1.Standard1['benchmark_${i}_${kbytes}'] ALL")
+    remote_cleanup
+    $(timed_cmd "set Keyspace1.Standard1['benchmark_${i}_${kbytes}']['${torrentColumns}'] = '${value}' ALL")
     torrentDuration=$(timed_cmd "get Keyspace1.Standard1['benchmark_${i}_${kbytes}'] ALL")
 
     echo -e "get\t$kbytes\t$normalDuration\t$torrentDuration"
